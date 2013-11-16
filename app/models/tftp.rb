9fa7522e48b9ae1d92ebf2ceb46ace50e5276691
zayas1988 <zayas1988@gmail.com>
2013-11-16 12:03:28 +0400

    tftp fix

--- app/models/tftp.rb
+++ app/models/tftp.rb
@@ -1,18 +1,10 @@
 class Tftp < ActiveRecord::Base
-<<<<<<< HEAD
   attr_accessible :config, :group, :image, :pathtoconfig, :pathtoimage, :configfilename
-=======
-<<<<<<< HEAD
-  attr_accessible :config, :group, :image, :pathtoconfig, :pathtoimage, :configfilename
-=======
-  attr_accessible :config, :group, :image, :pathtoconfig, :pathtoimage. :configfilename
->>>>>>> 44aa016923a542791ea88bb012c53f80b5f0fb18
->>>>>>> 78672329b0d06e30df641ba7751ec7aa8742eb75
   has_many :hosts
   validates(:image, presence: true)
   validates(:group, presence: true)
   validates(:config, presence: true)
-  validates(:pathtoconfig, presence: true)
-  validates(:pathtoimage, presence: true)
-  calidates(:configfilename, presence: true)
+#  validates(:pathtoconfig, presence: true)
+#  validates(:pathtoimage, presence: true)
+  validates(:configfilename, presence: true)
   end
